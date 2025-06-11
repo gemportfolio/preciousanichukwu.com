7c90353dfa2a712df28afb4ab13dd244aca5d1ad
Gem Portfolio <aydavidttg@gmail.com>
2025-06-11 08:39:57 -0700

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,71 @@
+const footer = document.querySelector(".footer");
+
+footer.innerHTML = `
+    <div class="container">
+        <div class="row pb-3">
+            <div class="col-lg-5 col-md-12 text">
+                <a class="fs-3 name" href="https://gemportfolio.github.io/affiliatedafisi">
+                    Precious Anichukwu
+                </a>
+                <p>
+                    I help business owners and personal brands attract clients, build authority, and sell with confidence through strategic content writing, persuasive copy, and brand storytelling.
+                </p>
+            </div>
+
+            <div class="col-lg-2 col-md-6">
+                <h5>Links</h5>
+                <ul class="list-unstyled mb-3">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a href="#testimonials">Testimonials</a>
+                    </li>
+                    <li>
+                        <a href="#hire">Hire Me</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-5 col-md-6 social">
+                <h5>Social Handles</h5>
+                <div class="">
+                    <a href="https://facebook.com/precious.chineyenwa.anichukwu">
+                        <i class="bi bi-facebook fs-3 pe-3"></i>
+                    </a>
+                    <a href="https://www.instagram.com/preciouschineyenwaanichukwu/">
+                        <i class="bi bi-instagram fs-3 pe-3"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/precious-anichukwu/">
+                        <i class="bi bi-linkedin fs-3"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="mx-auto" />
+    <div class="text-center copyright pt-2">
+        © Copyright 2025,
+        <a href="https://gemportfolio.github.io/affiliatedafisi" class="link"
+            >Precious Anichukwu</a
+        >
+    </div>
+    <div class="text-center copyright pb-3">
+        Designed by
+        <a href="https://thecreativeayd.com.ng/" class="link"
+            >TheCreativeAyd</a
+        >
+    </div>    
+    `;
+
+
+   
+       
+       
+       
---
+++ assets/js/navbar.js
@@ -0,0 +1,67 @@
+const nav = document.querySelector(".navbar");
+
+nav.innerHTML = `
+	<div class="container py-2">
+		<a class="navbar-brand" href="https://gemportfolio.github.io/affiliatedafisi">
+			<!--span><img src="./assets/img/logo.png"></span-->
+			Precious Anichukwu
+		</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ms-0 ms-auto">
+				<li class="nav-item my-auto">
+					<a class="nl nav-link" href="./index.html">Home</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nl nav-link" href="#about">About Me</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nl nav-link" href="#services">Services</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nl nav-link" href="#testimonials">Testimonials</a>
+				</li>
+				<li class="nav-item my-auto">
+					<a class="nl nav-link-btn lazy-el" href="#ebooks">
+						Buy My eBook
+						<i class="bi bi-cart ps-1"></i>
+					</a>
+				</li>
+				<div class="d-flex">
+					<li class="nav-item ct my-auto">
+						<a class="nav-icon" href="https://facebook.com/precious.chineyenwa.anichukwu">
+							<i class="bi bi-facebook"></i>
+						</a>
+					</li>
+					<li class="nav-item ct my-auto">
+						<a class="nav-icon" href="https://www.instagram.com/preciouschineyenwaanichukwu/">
+							<i class="bi bi-instagram"></i>
+						</a>
+					</li>
+					<li class="nav-item ct my-auto">
+						<a class="nav-icon" href="https://www.linkedin.com/in/precious-anichukwu/">
+							<i class="bi bi-linkedin"></i>
+						</a>
+					</li>
+				</div>
+			</ul>
+		</div>
+	</div>
+    `;
+
+
+   
+   
+   
+   
+   
